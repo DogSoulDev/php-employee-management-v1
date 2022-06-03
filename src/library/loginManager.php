b681449a0dfb6c690cf7073d6b44c50e2597956e
Javier Fernandez <dogsouldev@gmail.com>
2022-06-03 09:42:26 +0200

    test loginManager

--- src/library/loginManager.php
+++ src/library/loginManager.php
@@ -1,1 +1,21 @@
 <?php
+    $usersJson = file_get_contents(__DIR__."/../../resources/users.json");
+    $usersArray = json_decode($usersJson, true);
+
+    foreach ($usersArray['users'] as $user) {
+        $isValidUser = $user['email'] === $_POST['email'];
+        $isPasswordChecked = password_verify($_POST['password'], $user['password']);
+
+        if ($isValidUser && $isPasswordChecked) {
+
+            setcookie("userId", $user["userId"], time() + 84600, '/');
+            header("Location: http://localhost:8000/src/dashboard.php", true, 302);
+
+            exit();
+        }
+    }
+    
+    header("Location: http://localhost:8000/assets/html/unauthorized.html", true, 302);
+
+    exit();
+?>
