3363775cd8488db6fbd964f5a9d13c614c6c0169
Javier Fernandez <dogsouldev@gmail.com>
2022-06-06 00:11:04 +0200

    add logout

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,4 +1,21 @@
 <?php 
 echo "pepe";
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <a href="./library/loginController.php?logout"> <button type="submit" class="login-btn">Log in</button> </a>
+</body>
+
+</html>
--- src/library/loginController.php
+++ src/library/loginController.php
@@ -1,10 +1,18 @@
 <?php
 require './loginManager.php';
 
-checkLogin($_POST['username'], $_POST['password']);
+// checkLogin($_POST['username'], $_POST['password']);
 
 echo "<pre>";
-print_r($_POST['username']);
+print_r($_SERVER);
 echo "</pre>";
 
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    checkLogin($_POST['username'], $_POST['password']);
+}else if($_SERVER['QUERY_STRING'] == "logout"){
+    // logout();
+    unset($_SESSION);
+    header("location: ../../index.php");
+}
 ?>
