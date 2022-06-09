fe2e4f41eeeb5686b7687867fec176b407aa539a
Javier Fernandez <dogsouldev@gmail.com>
2022-06-09 09:30:07 +0200

    more sessionHelper trys

--- src/library/sessionHelper.php
+++ src/library/sessionHelper.php
@@ -1,11 +1,25 @@
 <?php
-if(time() - $_SESSION['timestamp'] > 10) { //subtract new timestamp from the old one
-    echo"<script>alert('15 Minutes over!');</script>";
-    unset($_SESSION['username'], $_SESSION['password'], $_SESSION['timestamp']);
-    $_SESSION['logged_in'] = false;
-    header("Location: http://127.0.0.1/php-employee-management-v1/src/library/logout.php/");//redirect to index.php
-    exit;
-} else {
-    $_SESSION['timestamp'] = time(); //set new timestamp
-}
+// !Dont delete
+// if(time() - $_SESSION['timestamp'] > 1000) { //subtract new timestamp from the old one
+//     echo"<script>alert('15 Minutes over!');</script>";
+//     unset($_SESSION['username'], $_SESSION['password'], $_SESSION['timestamp']);
+//     $_SESSION['logged_in'] = false;
+//     header("Location: http://127.0.0.1/php-employee-management-v1/src/library/logout.php/");//redirect to index.php
+//     exit;
+// } else {
+//     $_SESSION['timestamp'] = time(); //set new timestamp
+// }
+
+// if(isset($_SESSION['startTime'])) {
+// $logoutBtn = $_SESSION['startTime'] + (60 * 15);
+// if(time(600) > 
+// $logoutBtn) {
+//     session_destroy();
+//     header("Location: http://127.0.0.1/php-employee-management-v1/src/library/logout.php/ ");
+//     exit;
+// }
+// }
+
+
+
 ?>
